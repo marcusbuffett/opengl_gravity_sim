fb7c0c95586c06bdd01987e35848cc188368dbec
Marcus Buffett <marcusbuffett@me.com>
2015-12-20 15:00:09 -0500

    Renderer now renders a system with depth buffering

--- src/Renderer.hs
+++ src/Renderer.hs
@@ -3,10 +3,60 @@ module Renderer where
 import Simulation
 import System
 import Body
+import Debug.Trace
+import Util
+import Graphics.UI.GLUT as G
+import qualified Data.Vector.V3 as V
+import System.Random
+import Control.Monad
 
-import Graphics.UI.GLUT
 
 renderBody :: Body -> IO ()
 renderBody body = do
   color $ bColor body
-  renderObject Wireframe (Sphere' (bRadius body) 20 10)
+  preservingMatrix $ do 
+    translate (vectorToGLVector $ bPos body :: G.Vector3 GLfloat)
+    rotate (realToFrac $ bRot body) (vectorToGLVector $ bRotAngle body :: G.Vector3 GLfloat)
+    translate (vectorToGLVector . negate $ bPos body :: G.Vector3 GLfloat)
+    renderPrimitive Triangles $ 
+      forM_ (octahedron (bPos body) (bRadius body)) vertex
+    color $ G.Color3 0 0 (0 :: GLfloat)
+    renderPrimitive Lines $
+      forM_ (octahedronLines (bPos body) (bRadius body)) vertex
+
+renderSystem :: System -> IO ()
+renderSystem system = mapM_ renderBody (sBodies system)
+
+octahedron :: V.Vector3 -> Double -> [Vertex3 GLfloat] 
+octahedron pos s = map (vectorToVertex . (+ pos))
+                               [V.Vector3 w    0 0, V.Vector3 0 w    0, V.Vector3 0 0 w     ,
+                                V.Vector3 w    0 0, V.Vector3 0 w    0, V.Vector3 0 0 (-w)  ,
+                                V.Vector3 w    0 0, V.Vector3 0 (-w) 0, V.Vector3 0 0 w     ,
+                                V.Vector3 w    0 0, V.Vector3 0 (-w) 0, V.Vector3 0 0 (-w)  ,
+                                V.Vector3 (-w) 0 0, V.Vector3 0 w    0, V.Vector3 0 0 w     ,
+                                V.Vector3 (-w) 0 0, V.Vector3 0 w    0, V.Vector3 0 0 (-w)  ,
+                                V.Vector3 (-w) 0 0, V.Vector3 0 (-w) 0, V.Vector3 0 0 w     ,
+                                V.Vector3 (-w) 0 0, V.Vector3 0 (-w) 0, V.Vector3 0 0 (-w)]
+                    where
+                      w = realToFrac s
+
+octahedronLines :: V.Vector3 -> Double -> [Vertex3 GLfloat] 
+octahedronLines pos s = map (vectorToVertex . (+ pos))
+                               [V.Vector3 w    0  0 , V.Vector3 0 w    0 ,
+                                V.Vector3 w    0  0 , V.Vector3 0 (-w) 0 ,
+                                V.Vector3 w    0  0 , V.Vector3 0      0 w       ,
+                                V.Vector3 w    0  0 , V.Vector3 0      0 (-w)    ,
+                                V.Vector3 (-w) 0  0 , V.Vector3 0 w    0 ,
+                                V.Vector3 (-w) 0  0 , V.Vector3 0 (-w) 0 ,
+                                V.Vector3 (-w) 0  0 , V.Vector3 0      0 w       ,
+                                V.Vector3 (-w) 0  0 , V.Vector3 0      0 (-w),
+                                V.Vector3 0 w  0 , V.Vector3 0 0    w ,
+                                V.Vector3 0 w  0 , V.Vector3 0 0 (-w) ,
+                                V.Vector3 0 w  0 , V.Vector3 0 0    w ,
+                                V.Vector3 0 w  0 , V.Vector3 0 0 (-w) ,
+                                V.Vector3 0 (-w)  0 , V.Vector3 0 0    w ,
+                                V.Vector3 0 (-w)  0 , V.Vector3 0 0 (-w) ,
+                                V.Vector3 0 (-w)  0 , V.Vector3 0 0    w ,
+                                V.Vector3 0 (-w)  0 , V.Vector3 0 0 (-w)]
+                    where
+                      w = realToFrac s
