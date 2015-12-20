34cc45b6c77acf87487f81a769d412f5e8316832
Marcus Buffett <marcusbuffett@me.com>
2015-12-20 14:56:47 -0500

    Removed keyboardMouse

--- src/Bindings.hs
+++ src/Bindings.hs
@@ -1,4 +1,4 @@
-module Bindings (idle, display, reshape, keyboardMouse) where
+module Bindings (idle, display, reshape) where
 
 import Graphics.UI.GLUT
 import Data.IORef
@@ -6,15 +6,3 @@ import Display
  
 reshape :: ReshapeCallback
 reshape size = viewport $= (Position 0 0, size)
- 
-keyboardMouse :: KeyboardMouseCallback
-{- keyboardMouse key Down _ _ = case key of -}
-  {- (Char ' ') -> a $~! negate -}
-  {- (Char '+') -> a $~! (* 2) -}
-  {- (Char '-') -> a $~! (/ 2) -}
-  {- (SpecialKey KeyLeft ) -> p $~! \(x,y) -> (x-0.1,y) -}
-  {- (SpecialKey KeyRight) -> p $~! \(x,y) -> (x+0.1,y) -}
-  {- (SpecialKey KeyUp   ) -> p $~! \(x,y) -> (x,y+0.1) -}
-  {- (SpecialKey KeyDown ) -> p $~! \(x,y) -> (x,y-0.1) -}
-  {- _ -> return () -}
-keyboardMouse _ _ _ _ = return ()
