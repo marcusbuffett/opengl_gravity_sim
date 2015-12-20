51c8aebe25d12559f2bd25f7e63e10789be7fe67
Marcus Buffett <marcusbuffett@me.com>
2015-12-20 14:59:06 -0500

    Added depth, removed unused code from tut

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,22 @@
 import Graphics.UI.GLUT
 import Display
 import Data.IORef
+import System
 import Bindings
+import Body
+import Data.Vector.Class
+import Data.Time.Clock
+import Data.Vector.V3 as V
+import SolarSystem
 
-
-myPoints :: [(GLfloat,GLfloat,GLfloat)]
-myPoints = [ (sin (2*pi*k/24), cos (2*pi*k/24), 0) | k <- [1..24] ]
- 
 main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
-  initialDisplayMode $= [DoubleBuffered]
-  _window <- createWindow "Hello World"
+  initialDisplayMode $= [WithDepthBuffer, DoubleBuffered]
+  _window <- createWindow "Universe"
   reshapeCallback $= Just reshape
-  angle <- newIORef 0
-  delta <- newIORef 0.1
-  pos <- newIORef (0, 0)
-  keyboardMouseCallback $= Just (keyboardMouse delta pos)
-  idleCallback $= Just (idle angle pos)
-  displayCallback $= display angle pos
+  time <- getCurrentTime
+  systemRef <- solarSystem >>= newIORef
+  idleCallback $= Just idle
+  displayCallback $= display systemRef
   mainLoop
