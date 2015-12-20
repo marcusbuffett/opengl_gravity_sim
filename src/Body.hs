84293fb86bc9260334fb067cabc1835dc2c951ce
Marcus Buffett <marcusbuffett@me.com>
2015-12-20 14:20:35 -0500

    Used Vectors instead of tuples, added rotation

--- src/Body.hs
+++ src/Body.hs
@@ -1,12 +1,16 @@
-module Body where
+module Body (Body (..)) where
 
-import Graphics.UI.GLUT
-import Graphics.UI.GLUT.Objects
+import qualified Graphics.UI.GLUT as G
+import Data.Vector.V3 as V
 
-data Body = Body {bRadius :: Double, 
-                  bPos :: (Double, Double), 
-                  bVel :: Double,
-                  bDensity :: Double,
-                  bColor :: Color3 Double}
+data Body = Body {bRadius   :: Double,
+                  bPos      :: V.Vector3,
+                  bVel      :: V.Vector3,
+                  bAcc      :: V.Vector3,
+                  bRot      :: Double,
+                  bRotAngle :: V.Vector3,
+                  bRotSpeed :: Double,
+                  bMass     :: Double,
+                  bColor    :: G.Color3 G.GLfloat }
                   deriving (Show, Eq)
 
