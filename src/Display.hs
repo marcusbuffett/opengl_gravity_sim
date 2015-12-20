7d5a2918feaef67a09218b2ff821d925662c9903
Marcus Buffett <marcusbuffett@me.com>
2015-12-20 14:58:33 -0500

    Display now uses system ref

--- src/Display.hs
+++ src/Display.hs
@@ -4,27 +4,23 @@ import Graphics.UI.GLUT
 import Data.IORef
 import Control.Monad
 import Body
-import Cube
-import Points
+import System
+import Renderer
+import Data.Vector.V3
+import Data.Vector.Class
+import Simulation
+import Debug.Trace
+import Data.Time.Clock
 
 display :: IORef System -> DisplayCallback
-display System = do
-  clear [ColorBuffer]
+display systemRef = do
+  clear [ColorBuffer, DepthBuffer]
+  time <- getCurrentTime
+  modifyIORef systemRef (\sys -> simulate sys time)
   loadIdentity
-  (x',y') <- get pos
-  translate $ Vector3 x' y' 0
-  renderBody $ Body 1 (0, 0) 1.0 (Color3 1 0 1)
-  preservingMatrix $ do
-    a <- get angle
-    rotate a $ Vector3 0 0 1
-    scale 0.7 0.7 (0.7::GLfloat)
-    forM_ (points 7) $ \(x,y,z) -> preservingMatrix $ do
-      color $ Color3 ((x+1)/2) ((y+1)/2) ((z+1)/2)
-      translate $ Vector3 x y z
-      cube 0.1
+  depthFunc $= Just Less
+  readIORef systemRef >>= renderSystem 
   swapBuffers
 
-idle :: IORef GLfloat -> IORef (GLfloat, GLfloat) -> DisplayCallback
-idle angle pos = do
-  angle $~! (+ 0.1)
-  postRedisplay Nothing
+idle :: DisplayCallback
+idle = postRedisplay Nothing
