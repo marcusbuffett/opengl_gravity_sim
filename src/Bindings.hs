2af90c9231fc9bc61203c1963590ddde9b0be376
Marcus Buffett <marcusbuffett@me.com>
2015-12-20 14:19:58 -0500

    Refactored reshape, removed keyboardMouse

--- src/Bindings.hs
+++ src/Bindings.hs
@@ -5,17 +5,16 @@ import Data.IORef
 import Display
  
 reshape :: ReshapeCallback
-reshape size = do 
-  viewport $= (Position 0 0, size)
+reshape size = viewport $= (Position 0 0, size)
  
-keyboardMouse :: IORef GLfloat -> IORef (GLfloat, GLfloat) -> KeyboardMouseCallback
-keyboardMouse a p key Down _ _ = case key of
-  (Char ' ') -> a $~! negate
-  (Char '+') -> a $~! (* 2)
-  (Char '-') -> a $~! (/ 2)
-  (SpecialKey KeyLeft ) -> p $~! \(x,y) -> (x-0.1,y)
-  (SpecialKey KeyRight) -> p $~! \(x,y) -> (x+0.1,y)
-  (SpecialKey KeyUp   ) -> p $~! \(x,y) -> (x,y+0.1)
-  (SpecialKey KeyDown ) -> p $~! \(x,y) -> (x,y-0.1)
-  _ -> return ()
-keyboardMouse _ _ _ _ _ _ = return ()
+keyboardMouse :: KeyboardMouseCallback
+{- keyboardMouse key Down _ _ = case key of -}
+  {- (Char ' ') -> a $~! negate -}
+  {- (Char '+') -> a $~! (* 2) -}
+  {- (Char '-') -> a $~! (/ 2) -}
+  {- (SpecialKey KeyLeft ) -> p $~! \(x,y) -> (x-0.1,y) -}
+  {- (SpecialKey KeyRight) -> p $~! \(x,y) -> (x+0.1,y) -}
+  {- (SpecialKey KeyUp   ) -> p $~! \(x,y) -> (x,y+0.1) -}
+  {- (SpecialKey KeyDown ) -> p $~! \(x,y) -> (x,y-0.1) -}
+  {- _ -> return () -}
+keyboardMouse _ _ _ _ = return ()
