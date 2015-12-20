70e90acbf9c578e7335eca9f42b81f0e6f48976e
Marcus Buffett <marcusbuffett@me.com>
2015-12-20 15:00:53 -0500

    Changed type of last update field, added scale fn

--- src/System.hs
+++ src/System.hs
@@ -1,9 +1,25 @@
 module System where
 
 import Body
+import Data.Time.Clock
+import Data.Vector.Class
 
 data System = System {sBodies :: [Body], 
                       sSpeed :: Float, 
                       sGravityAmplifier :: Float,
-                      sLastUpdate :: Double} 
+                      sLastUpdate :: UTCTime} 
+                      deriving (Show)
 
+instance Show UTCTime where
+  show time = ""
+
+scaleSystem :: Double -> System -> System
+scaleSystem factor system = system {sBodies = map (scaleBody factor) $ sBodies system} 
+
+scaleBody :: Double -> Body -> Body
+scaleBody factor body = body {
+    bRadius = bRadius body * factor,
+    bPos = bPos body |* factor,
+    bVel = bVel body |* factor,
+    bAcc = bAcc body |* factor,
+    bMass = bMass body * factor}
